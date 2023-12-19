6558228b13977dd7568bcc3a7cd7fff280d07145
yulia <koroleva21.jv@gmail.com>
2023-12-19 21:56:11 +0300

    fix

--- Source/Adventure_Chest/Adventure_Chest/DB/Connect.cs
+++ Source/Adventure_Chest/Adventure_Chest/DB/Connect.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using Npgsql;
+using Microsoft.Extensions.Configuration;
 namespace Adventure_Chest.DB
 {
 	public class Connect
 	{
-		public Connect()
+        private static IConfiguration Configuration { get; set; }
+        static Connect()
 		{
-		}
-	}
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("connect.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+        public Connect()
+        {
+            string SqlConnectionString = Configuration["SqlConnect"];
+            // остальной код
+        }
+    }
 }
 
