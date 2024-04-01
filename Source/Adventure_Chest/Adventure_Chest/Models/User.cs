ec6a605fea976806c9a0eaa6dcdc1ffeedc1ca12
yulia <koroleva21.jv@gmail.com>
2024-04-01 15:52:43 +0300

    fix

--- Source/Adventure_Chest/Adventure_Chest/Models/User.cs
+++ Source/Adventure_Chest/Adventure_Chest/Models/User.cs
@@ -4,8 +4,10 @@ namespace Adventure_Chest.Models
 	public class User
 	{
         public int ID { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
+        public string Login { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public DateTime Birthday { get; set; }
 
         public User()
